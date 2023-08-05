c85313dd03e88e48c13dc7d5fc0442adfd755507
anoopoo7 <anoopviswanadh007@gmail.com>
2023-08-06 03:08:07 +0530

    controller advices added

---
+++ src/main/java/com/lxiya/xendercart/category/controller/CategoryControllerAdvices.java
@@ -0,0 +1,45 @@
+/*
+*   Copyright © 2023
+*   This software is the intellectual property of Lxiyas. 
+*   Unauthorized use, reproduction, or distribution is strictly prohibited. 
+*
+*   Anoop ss
+*/
+package com.lxiya.xendercart.category.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.lxiya.xendercart.core.exceptionHandling.ExceptionResponse;
+
+@ControllerAdvice
+@Configuration
+public class CategoryControllerAdvices {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleBadRequestException(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getDefaultMessage())
+                .collect(Collectors.toList());
+        return new ResponseEntity<ExceptionResponse>(
+                new ExceptionResponse(HttpStatus.BAD_REQUEST.getReasonPhrase(), errors),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleException(Exception ex) {
+        return new ResponseEntity<ExceptionResponse>(
+                new ExceptionResponse(HttpStatus.CONFLICT.getReasonPhrase(), List.of(ex.getMessage())),
+                HttpStatus.CONFLICT);
+    }
+
+}
---
+++ src/main/java/com/lxiya/xendercart/core/exceptionHandling/ExceptionResponse.java
@@ -0,0 +1,24 @@
+/*
+*   Copyright © 2023
+*   This software is the intellectual property of Lxiyas. 
+*   Unauthorized use, reproduction, or distribution is strictly prohibited. 
+*
+*   Anoop ss
+*/
+package com.lxiya.xendercart.core.exceptionHandling;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExceptionResponse {
+
+    private String errorCode;
+
+    private List<String> message;
+}
