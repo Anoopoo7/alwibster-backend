25f0f971c070d4af47dcfcc71951211c2cf4d9f8
Anoop SS <69832338+Anoopoo7@users.noreply.github.com>
2023-09-07 14:45:12 +0530

    repo page codes

--- src/main/java/com/lxiya/xendercart/product/persistance/repository/impl/SkuRepositoryCustomImpl.java
+++ src/main/java/com/lxiya/xendercart/product/persistance/repository/impl/SkuRepositoryCustomImpl.java
@@ -15,3 +15,36 @@ import com.lxiya.xendercart.product.persistance.repository.SkuRepositoryCustom;
 public class SkuRepositoryCustomImpl implements SkuRepositoryCustom {
 
 }
+// long count = mongoTemplate.count(query, Contract.class);
+//         query.with(pageRequest);
+//         List<Contract> result = mongoTemplate.find(query, Contract.class);
+//         return PageableExecutionUtils.getPage(result, pageRequest, () -> count);
+
+
+
+// @Data
+// @AllArgsConstructor
+// @NoArgsConstructor
+// public class PageView {
+
+//     public PageView(final Page<?> page, final Collection<?> content) {
+//         this.setContent(content);
+//         this.setTotalElements(page.getTotalElements());
+//         this.setTotalPages(page.getTotalPages());
+//         this.setNumberOfElements(page.getNumberOfElements());
+//         this.setEndOfResult(page.isLast());
+//         this.setStartOfResult(page.isFirst());
+//     }
+
+//     private Collection<?> content;
+
+//     private long totalElements;
+
+//     private int totalPages;
+
+//     private int numberOfElements;
+
+//     private boolean endOfResult;
+
+//     private boolean startOfResult;
+// }
